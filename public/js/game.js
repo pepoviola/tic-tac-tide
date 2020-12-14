41aaabffa508151ec9651ff5420f7a015712e4e3
Pepo Viola <pepoviola@gmail.com>
2020-12-14 20:39:18 -0300

    initial version

---
+++ public/js/game.js
@@ -0,0 +1,174 @@
+let io;
+document.addEventListener("DOMContentLoaded", function() {
+    // connect to ws
+    io = new WebSocket( `${window.location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}${window.location.pathname}` );
+
+    io.addEventListener('message', message => {
+        console.log('Message from server', message.data);
+        const data = JSON.parse(message.data);
+        switch( data.cmd ) {
+            case 'STATE':
+                gameState = data.play_book;
+                // redraw
+                redrawPlayBook();
+                if( ! handleResultValidation() ) handlePlayerChange();
+                break;
+            case 'INIT':
+                console.log( 'init' );
+                localPlayer = data.player == "0" ? "X" : "O";
+                gameState = data.play_book;
+                // redraw
+                redrawPlayBook();
+                handlePlayerChange();
+                gameActive = localPlayer === currentPlayer ? true : false;
+                break;
+            case 'RESET':
+                console.log( 'reset' );
+                if( ! reset_by_me ) alert( 'Other player just RESET the game' );
+
+                gameState = data.play_book;
+                // redraw
+                redrawPlayBook();
+                handlePlayerChange();
+                gameActive = localPlayer === currentPlayer ? true : false;
+                reset_by_me = false;
+                break;
+            case 'COMPLETE':
+                console.log( 'COMPLETE' );
+                alert( 'Oops... board complete!' );
+                statusDisplay.innerHTML = COMPLETE_MSG;
+                statusDisplay.classList.add("complete");
+                document.querySelector('.game--restart').classList.add("hidden");
+                gameActive = false;
+                break;
+            case 'leave':
+                console.log( 'leave' );
+                break;
+        };
+    });
+});
+
+//
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn. <span class="game--player">(You are ${localPlayer})</span>`;
+const COMPLETE_MSG = `Board complete! please <a href="/">home</a> and create or join another board`
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+
+let gameActive = false;
+let currentPlayer = "";
+let localPlayer = "";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+let reset_by_me = false;
+
+function init() {
+    io.send('play');
+}
+
+
+const statusDisplay = document.querySelector('.game--status');
+statusDisplay.innerHTML = `Initializing...`;
+
+function handleCellPlayedLocally(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    //clickedCell.innerHTML = currentPlayer;
+    clickedCell.classList.add(currentPlayer.toLocaleLowerCase());
+    io.send( `PLAY:${localPlayer}:${clickedCellIndex }` );
+    gameActive = false;
+}
+
+function redrawPlayBook() {
+    document.querySelectorAll('.cell').forEach( cell => {
+        const play = gameState[ cell.dataset.cellIndex ];
+        if( play !== "" ) cell.classList.add(play.toLocaleLowerCase());
+        else cell.classList.remove( "x", "o" );
+    });
+}
+
+function handlePlayerChange() {
+    const plays = gameState.filter( x => x !== '' );
+    currentPlayer = plays.length % 2 === 0 ? "X" : "O";
+    statusDisplay.innerHTML = currentPlayerTurn();
+
+    gameActive = currentPlayer === localPlayer;
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    let winCondition;
+    for (let i = 0; i <= 7; i++) {
+        winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        cells = document.querySelectorAll( '.cell' );
+        winCondition.forEach( index => {
+            var cell = cells[ index ];
+            cell.classList.add("win");
+        });
+
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return true;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return true;
+    }
+
+    // handlePlayerChange();
+    // io.send('play');
+
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayedLocally(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    reset_by_me = true;
+    io.send( `RESET:${localPlayer}` );
+    // gameState = ["", "", "", "", "", "", "", "", ""];
+    // statusDisplay.innerHTML = currentPlayerTurn();
+    // document.querySelectorAll('.cell').forEach(cell => {
+    //     cell.classList.remove("x". "o");
+    // });
+}
+
+
+// init client
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
---
+++ src/main.rs
@@ -0,0 +1,254 @@
+use async_std::prelude::*;
+
+use std::collections::hash_map::{Entry,HashMap};
+use async_std::sync::{Arc, RwLock};
+use broadcaster::BroadcastChannel;
+use futures_util::future::Either;
+use futures_util::StreamExt;
+use serde_derive::Serialize;
+use serde_json::json;
+use tide::http::format_err;
+use tide::Body;
+use tide::Request;
+// use tide_websockets::{WebSocketConnection, async_tungstenite::tungstenite::{Message as WSMessage}};
+// use tide_websockets::WebsocketMiddleware;
+use tide_websockets::{Message as WSMessage, WebSocket, WebSocketConnection};
+
+#[derive(Clone, Debug, Serialize)]
+enum Message {
+    Command { cmd: String, play_book: [String;9] }
+}
+
+#[derive(Clone)]
+struct Board {
+    id: String, // id of the board
+    play_book: [String;9], // Arc<RwLock<[String;9]>>,
+    wscs: Vec<WebSocketConnection> //BroadcastChannel<Message>
+}
+
+
+#[derive(Clone)]
+struct State {
+    broadcaster: BroadcastChannel<Message>,
+    boards: Arc<RwLock<HashMap<String,Board>>>,
+    play_book: Arc<RwLock<[String;9]>>,
+}
+
+impl State {
+    fn new() -> Self {
+        let plays:[String;9] = Default::default();
+        Self {
+            broadcaster: BroadcastChannel::new(),
+            boards: Default::default(),
+            play_book: Arc::new(RwLock::new(plays)),
+        }
+    }
+
+    async fn add_player_to_board(&self, board_id: &str, wsc: WebSocketConnection ) -> Result<(usize,[String;9]),String> { // tide::Result<()> {
+        let mut boards = self.boards.write().await;
+        match boards.entry(board_id.to_owned()) {
+            Entry::Vacant(_) => {
+                let b = Board {
+                    id : board_id.to_owned(),
+                    play_book : Default::default(),
+                    wscs : vec![wsc],
+                };
+                boards.insert(board_id.to_owned(), b );
+                Ok((0,boards.get(board_id).unwrap().play_book.clone()))
+            },
+            Entry::Occupied(mut board) => {
+                // check if we had the two players
+                let mut b = board.get_mut().wscs.clone();
+                if b.len() < 2 {
+                    b.push( wsc );
+                    board.get_mut().wscs = b;
+                    Ok((1,board.get().play_book.clone()))
+                } else {
+                    return Err(String::from("COMPLETE"))
+                }
+            }
+        }
+    }
+
+    async fn make_play_in_board(&self, board_id: &str, player_label: String,  cell_index: usize) -> tide::Result<()> {
+
+        let mut boards = self.boards.write().await;
+        let mut board = boards.get_mut(board_id).unwrap();
+        let mut pb = board.play_book.clone();
+        pb[cell_index] = player_label;
+
+        board.play_book = pb.clone();
+
+        drop(boards);
+
+        let cmd = String::from("STATE");
+        println!("{} to send state", board_id);
+        self.send_message(board_id, Message::Command{ cmd, play_book : pb }).await
+    }
+
+    async fn reset_board( &self, board_id: &str) -> tide::Result<()> {
+        let mut boards = self.boards.write().await;
+        let mut board = boards.get_mut(board_id).unwrap();
+
+
+        board.play_book = Default::default();
+
+        let cmd = String::from("RESET");
+        self.send_message(board_id, Message::Command{ cmd, play_book : Default::default() }).await
+    }
+
+
+    async fn send_message(&self, board_id: &str, message: Message) -> tide::Result<()> {
+        let mut boards = self.boards.write().await;
+        match boards.entry(board_id.to_owned()) {
+            Entry::Vacant(_) => {
+                println!("{} vacant", board_id);
+            },
+            Entry::Occupied(mut board) => {
+
+                match message {
+                    Message::Command { cmd, play_book } => {
+                        println!("{} messages", board_id);
+                        for wsc in &board.get_mut().wscs {
+                            println!("{} message {}", board_id, cmd);
+                            wsc.send_json(&json!({
+                                "cmd": cmd,
+                                "play_book" : play_book.clone()
+                            })).await?
+                        }
+                    }
+                };
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    let mut app = tide::with_state(State::new());
+    app.with(tide::sessions::SessionMiddleware::new(
+        tide::sessions::MemoryStore::new(),
+        std::env::var("SESSION_SECRET").unwrap().as_bytes(),
+    ));
+
+    let mut state = app.state().clone();
+    async_std::task::spawn(async move {
+        while let Some(message) = state.broadcaster.next().await {
+            match message {
+                Message::Command { cmd, play_book } => println!("Cmd - {}: {:?}", cmd, play_book),
+            };
+        }
+        tide::Result::Ok(())
+    });
+
+    app.at("/:id")
+        // .with(WebsocketMiddleware::new(
+        .with(WebSocket::new(
+            |request: Request<State>, wsc| async move {
+                let key = request.param("id")?;
+
+                let state = request.state().clone();
+                let broadcaster = state.broadcaster.clone();
+
+                let mut combined_stream = futures_util::stream::select(
+                    wsc.clone().map(|l| Either::Left(l)),
+                    broadcaster.clone().map(|r| Either::Right(r)),
+                );
+
+
+                match state.add_player_to_board(key, wsc.clone()).await {
+                    Ok( ( player, play_book) ) => {
+                        wsc.send_json(&json!({
+                            "cmd":"INIT",
+                            "player":player,
+                            "play_book" : play_book.clone()
+                        })).await?
+                    }
+                    Err(_) => {
+                        wsc.send_json(&json!({
+                            "cmd":"COMPLETE"
+                        })).await?
+                    }
+                }
+
+                while let Some(item) = combined_stream.next().await {
+                    match item {
+                        Either::Left(Ok(WSMessage::Text(message))) => {
+                            println!("{:?}", message);
+                            let parts: Vec<&str> = message.split(":").collect();
+
+                            match parts[0] {
+                                "LEAVE" => {},
+                                "PLAY" => {
+                                    state.make_play_in_board(key, parts[1].parse().unwrap(), parts[2].parse().unwrap()).await?;
+                                },
+                                "RESET" => {
+                                    state.reset_board(key).await?;
+                                }
+                                _ => println!( "INVALID message")
+                            }
+                            // if parts[0] == "LEAVE" {
+                            //     // state.remove_player(player_index.unwrap()).await?;
+                            // } else if parts[0] == "PLAY" {
+                            //     state.make_play(parts[1].parse().unwrap(), parts[2].parse().unwrap(), key).await?;
+                            // } else {
+                            //     println!( "INVALID message")
+                            // };
+                        }
+
+
+                        Either::Right(Message::Command { cmd, play_book }) => {
+                            wsc.send_json(
+                                &json!({ "cmd" : cmd, "play_book": play_book }),
+                            )
+                            .await?;
+                        }
+
+                        o => {
+                            log::debug!("{:?}", o);
+                            return Err(format_err!("no idea"));
+                        }
+                    }
+                }
+
+                Ok(())
+            },
+        ))
+    .get(|_| async { Ok(Body::from_file("./public/game.html").await?) });
+
+    app.at("/new").post(|_| async {
+        let petnames = petname::Petnames::default();
+        let board_name = petnames.generate_one(2, "-");
+        Ok( json!({ "board_name" :board_name}))
+    });
+
+    app.at("/random").post(|request: Request<State>| async move {
+        let mut board_name = String::from("");
+        let state = request.state().clone();
+        let mut boards = state.boards.write().await;
+
+        let board = boards.iter_mut().find(|x| {
+           x.1.wscs.len() == 1
+        });
+
+        if let Some(b) = board {
+            board_name = b.0.to_owned()
+        }
+
+        println!("board_name : {}", board_name);
+        Ok( json!({ "board_name" :board_name}))
+    });
+
+    app.at("/").get(|_| async { Ok(Body::from_file("./public/index.html").await?) });
+
+    app.at("/public").serve_dir("./public/")?;
+
+    app.listen("127.0.0.1:8080").await?;
+
+    Ok(())
+}
